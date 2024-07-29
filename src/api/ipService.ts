e7e6af93fb4bfa0b3de910c06e3543709a862b6b
Vlad <uladislau.shusterman.important@gmail.com>
2024-07-29 18:04:39 +0200

    ip-lookup | error messages, cleanups

--- src/api/ipService.ts
+++ src/api/ipService.ts
@@ -1,45 +1,46 @@
 import axios from 'axios';
 import { IpInfo } from '@/types/IpEntity';
+import { ErrorMessages } from '@/utils/constants';
 
 const API_URL = 'https://ipinfo.io/';
 
 export async function getIpData(ip: string): Promise<IpInfo> {
-    const endpoint = ip ? `${API_URL}/${ip}/json` : `${API_URL}/json`;
+    const endpoint = ip.trim() ? `${API_URL}/${ip}/json` : `${API_URL}/json`;
 
     try {
-        const { data } = await axios.get<IpInfo>(endpoint);
-        console.log(data);
-        return data;
-    } catch (error: any) {
-        if (axios.isAxiosError(error)) {
-            if (!error.response) {
-                console.error('Network error or no response received:', error.message);
-                throw new Error('Network error or no response received');
-            }
+      const { data } = await axios.get<IpInfo>(endpoint);
 
-            switch (error.response.status) {
-                case 400:
-                    console.error('Bad request:', error.response.data);
-                    throw new Error('Bad request');
-                case 401:
-                    console.error('Unauthorized:', error.response.data);
-                    throw new Error('Unauthorized');
-                case 403:
-                    console.error('Forbidden:', error.response.data);
-                    throw new Error('Forbidden');
-                case 404:
-                    console.error('Not found:', error.response.data);
-                    throw new Error('Not found');
-                case 500:
-                    console.error('Internal server error:', error.response.data);
-                    throw new Error('Internal server error');
-                default:
-                    console.error('Unexpected error:', error.response.data);
-                    throw new Error('Unexpected error occurred');
-            }
-        } else {
-            console.error('An unexpected error occurred:', error);
-            throw new Error('An unexpected error occurred');
+      return data;
+    } catch (error: any) {
+      if (axios.isAxiosError(error)) {
+        if (!error.response) {
+          console.error(ErrorMessages.NETWORK_ERROR, error.message);
+          throw new Error(ErrorMessages.NETWORK_ERROR);
         }
+
+        switch (error.response.status) {
+          case 400:
+            console.error(ErrorMessages.BAD_REQUEST, error.response.data);
+            throw new Error(ErrorMessages.BAD_REQUEST);
+          case 401:
+            console.error(ErrorMessages.UNAUTHORIZED, error.response.data);
+            throw new Error(ErrorMessages.UNAUTHORIZED);
+          case 403:
+            console.error(ErrorMessages.FORBIDDEN, error.response.data);
+            throw new Error(ErrorMessages.FORBIDDEN);
+          case 404:
+            console.error(ErrorMessages.NOT_FOUND, error.response.data);
+            throw new Error(ErrorMessages.NOT_FOUND);
+          case 500:
+            console.error(ErrorMessages.INTERNAL_SERVER_ERROR, error.response.data);
+            throw new Error(ErrorMessages.INTERNAL_SERVER_ERROR);
+          default:
+            console.error(ErrorMessages.UNEXPECTED_ERROR, error.response.data);
+            throw new Error(ErrorMessages.UNEXPECTED_ERROR);
+        }
+      } else {
+          console.error(ErrorMessages.UNEXPECTED_ERROR, error);
+          throw new Error(ErrorMessages.UNEXPECTED_ERROR);
+      }
     }
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,14 +3,21 @@ export const TimeFormat = 'HH:mm:ss';
 export const INTERVAL_TIME = 1000;
 export const IpValidationRegexp =  /^([0-9]{1,3}\.){3}[0-9]{1,3}$/;
   
-export const ErrorMessages = {
-    INVALID_IP: 'Invalid IP address format',
+export enum ErrorMessages {
+  INVALID_IP = 'Invalid IP address format',
+  NETWORK_ERROR = 'Network error or no response received',
+  BAD_REQUEST = 'Bad request',
+  UNAUTHORIZED = 'Unauthorized',
+  FORBIDDEN = 'Forbidden',
+  NOT_FOUND = 'Not found',
+  INTERNAL_SERVER_ERROR = 'Internal server error',
+  UNEXPECTED_ERROR = 'Unexpected error occurred',
 };
 
 export const Translations = {
-    EN: {
-        DESCRIPTION: 'Enter one or more IP addresses and get their country',
-        INPUT_PLACEHOLDER: 'Leave empty to get your current IP',
-        ADD_BUTTON_LABEL: 'Add',
-    }
-}
+  EN: {
+    DESCRIPTION: 'Enter one or more IP addresses and get their country',
+    INPUT_PLACEHOLDER: 'Leave empty to get your current IP',
+    ADD_BUTTON_LABEL: 'Add',
+  }
+};
